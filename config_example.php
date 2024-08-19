ea239b1fdd1d3b1f2068c41932718b390f5ec218
parkseunghan <01seunghan@gmail.com>
2024-08-20 01:12:20 +0900

    config_example.php

---
+++ config_example.php
@@ -0,0 +1,17 @@
+<?php
+// DB 설정
+define('DB_SERVER', 'your_servername');
+define('DB_USERNAME', 'your_username');
+define('DB_PASSWORD', 'your_password');
+define('DB_NAME', 'your_dbname');
+
+// 파일 업로드 경로
+define('UPLOAD_DIR', your_dir_path);
+
+// 파일 업로드 최대 크기
+define('MAX_FILE_SIZE', 5 * 1024 * 1024); // 5MB
+
+// 파일 업로드 허용 형식
+define('ALLOWED_EXTS', ['jpg', 'jpeg', 'png', 'gif', 'pdf']);
+?>
+
